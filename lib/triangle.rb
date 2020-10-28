3aeedab2a1ac08c9468ef44592cd954c1504ccf3
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-28 16:44:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,11 +8,11 @@ class Triangle
   end
 
   def kind
-    if @s1 <= 0 || @s2 <= 0 || @s3 <= 0
+    if @s1 <= 0 || @s2 <= 0 || @s3 <= 0 # Checks that all sides are a positive integer
 
         raise TriangleError
 
-    elsif @s1 + @s2 <= @s3 || @s1 + @s3 <= @s2 || @s2 + @s3 <= @s1
+    elsif @s1 + @s2 <= @s3 || @s1 + @s3 <= @s2 || @s2 + @s3 <= @s1 # Checks for trinangle inequality
 
         raise TriangleError
 
