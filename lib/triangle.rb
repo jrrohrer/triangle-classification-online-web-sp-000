48e605ed8b8b3cd31ea46d9c9d9bc265ba4f9502
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-28 16:38:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,11 +11,11 @@ class Triangle
     if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
       begin
         raise TriangleError
-      rescue PartnerError => error
-        puts error.message
       end
     elsif @side_1 + @side_2 <= @side_3 || @side_1 + @side_3 <= @side_2 || @side_2 + @side_3 <= @side_1
-      raise TriangleError
+      begin
+        raise TriangleError
+      end
     else
       if @side_1 == @side_2 && @side_2 == @side_3
         :equilateral
@@ -27,10 +27,9 @@ class Triangle
     end
   end
 
-end
-
-class TriangleError < StandardError
-  def message
-    "This is not a legal triangle."
+  class TriangleError < StandardError
+    
   end
 end
+
+
