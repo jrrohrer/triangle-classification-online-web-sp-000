9d931b1c233aaf1eb68a18b0dd6ec62ddf60207f
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-28 16:36:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,24 +7,30 @@ class Triangle
     @s3 = s3
   end
 
-  #def kind
-    #if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
-      #raise TriangleError
-    #elsif @side_1 + @side_2 <= @side_3 || @side_1 + @side_3 <= @side_2 || @side_2 + @side_3 <= @side_1
-      #raise TriangleError
-    #else
-      #if @side_1 == @side_2 && @side_2 == @side_3
-        #:equilateral
-      #elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
-        #:isosceles
-      #elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
-        #:scalene
-      #end
-    #end
-  #end
+  def kind
+    if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
+      begin
+        raise TriangleError
+      rescue PartnerError => error
+        puts error.message
+      end
+    elsif @side_1 + @side_2 <= @side_3 || @side_1 + @side_3 <= @side_2 || @side_2 + @side_3 <= @side_1
+      raise TriangleError
+    else
+      if @side_1 == @side_2 && @side_2 == @side_3
+        :equilateral
+      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+        :isosceles
+      elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
+        :scalene
+      end
+    end
+  end
 
 end
 
 class TriangleError < StandardError
-
+  def message
+    "This is not a legal triangle."
+  end
 end
