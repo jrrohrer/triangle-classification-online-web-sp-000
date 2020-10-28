1fd20eb253a342b52993179301e6f35e1dbcb968
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-28 16:19:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
         :equilateral
       elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
         :isosceles
-      elsif @side_1 != @side_2 && @side_2 !- @side_3 && @side_1 != @side_3
+      elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
         :scalene
       end
     end
